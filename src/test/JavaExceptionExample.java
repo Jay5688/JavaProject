9aba80812753a89775dbb0e10159942c66b885f4
Jay5688 <kishorev5688@gmail.com>
2022-12-07 13:02:10 +0530

    Commit to main branch

--- src/test/JavaExceptionExample.java
+++ src/test/JavaExceptionExample.java
@@ -8,15 +8,15 @@ public class JavaExceptionExample{
              //a[5]=30/0;    
              System.out.println(a[10]);  
             }    
-        
+        catch(ArithmeticException e)  
+        {  
+         System.out.println("Arithmetic Exception occurs");  
+        }
         catch(Exception e)  
         {  
          System.out.println("Parent Exception occurs");  
         }   
-            catch(ArithmeticException e)  
-               {  
-                System.out.println("Arithmetic Exception occurs");  
-               }    
+                
             /*catch(ArrayIndexOutOfBoundsException e)  
                {  
                 System.out.println("ArrayIndexOutOfBounds Exception occurs");  
